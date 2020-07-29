a0bcb545959d639e124466ffc41d51cec9f14fc3
Suchi Chaudhary <Suchi.Chaudhary@Perpello.se>
2020-07-29 19:18:36 +0200

    DbContext

--- Persistence/CollegeDbContext.cs
+++ Persistence/CollegeDbContext.cs
@@ -1,7 +1,20 @@
+using College.Services.Entities;
+using Microsoft.EntityFrameworkCore;
+
 namespace College.Services.Persistence
 {
-    public class CollegeDbContext
+
+    public class CollegeDbContext : DbContext
     {
-        
+
+        public CollegeDbContext(DbContextOptions<CollegeDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Professor> Professors { get; set; }
+
+        public DbSet<Student> Students { get; set; }
     }
+
 }
