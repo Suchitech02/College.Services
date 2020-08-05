679c18a8a7869b1d301147cd6e53759dd7e90780
Suchi Chaudhary <Suchi.Chaudhary@Perpello.se>
2020-08-05 18:53:06 +0200

    Controller

--- Controllers/ProfessorsController.cs
+++ Controllers/ProfessorsController.cs
@@ -1,7 +1,71 @@
+using College.Services.BAL;
+using College.Services.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
 namespace College.Services.Controllers
-{
-    public class ProfessorsController
+{ [Route("api/[controller]")]
+    [ApiController]
+    public class ProfessorsController : ControllerBase
     {
-        
+        private readonly ProfessorsBal _professorsBusiness;
+
+        public ProfessorsController(ProfessorsBal professorsBusiness)
+        {
+            _professorsBusiness = professorsBusiness;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Professor>> Get()
+        {
+            var professors = _professorsBusiness.GetProfessors();
+
+            return Ok(professors);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Professor> GetProfessorById(int id)
+        {
+            var professor = _professorsBusiness.GetProfessorById(id);
+
+            if (professor == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(professor);
+        }
+
+        [HttpPost]
+        public ActionResult<Professor> AddProfessor([FromBody]Professor professor)
+        {
+            var createdProfessor = _professorsBusiness.AddProfessor(professor);
+
+            return Created(string.Empty, createdProfessor);
+        }
+
+        [HttpPut]
+        public ActionResult UpdateProfessor([FromBody]Professor professor)
+        {
+            var _ = _professorsBusiness.UpdateProfessor(professor);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult DeleteProfessor(int  id)
+        {
+            var professorDeleted = _professorsBusiness.DeleteProfessorById(id);
+
+            if (!professorDeleted)
+            {
+                return StatusCode(500, $"Unable to delete Professor with id {id}");
+            }
+
+            return NoContent();
+        }
+
     }
+
 }
